c4d25a0f74f3ff0ee1cfa6ececbe73752442028e
aman2698 <sikarwaraman26@gmail.com>
2021-08-26 10:58:32 +0530

    first commit

---
+++ src/components/navigation.jsx
@@ -0,0 +1,68 @@
+export const Navigation = (props) => {
+  return (
+    <nav id='menu' className='navbar navbar-default navbar-fixed-top' style={{backgroundColor:"#2b3b4c"}}>
+      <div className='container' >
+        <div className='navbar-header'>
+          <button
+            type='button'
+            className='navbar-toggle collapsed'
+            data-toggle='collapse'
+            data-target='#bs-example-navbar-collapse-1'
+          >
+            {' '}
+            <span className='sr-only'>Toggle navigation</span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+            <span className='icon-bar'></span>{' '}
+          </button>
+          <a className='navbar-brand page-scroll' href='#page-top' style={{color:"floralwhite"}}>
+            React Landing Page
+          </a>{' '}
+        </div>
+
+        <div
+          className='collapse navbar-collapse'
+          id='bs-example-navbar-collapse-1'
+        >
+          <ul className='nav navbar-nav navbar-right'>
+            <li>
+              <a href='#features' className='page-scroll ' style={{color:"floralwhite"}}>
+                Features
+              </a>
+            </li>
+            <li>
+              <a href='#about' className='page-scroll' style={{color:"floralwhite"}}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#services' className='page-scroll' style={{color:"floralwhite"}}>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href='#portfolio' className='page-scroll' style={{color:"floralwhite"}}>
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href='#testimonials' className='page-scroll' style={{color:"floralwhite"}}>
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href='#team' className='page-scroll' style={{color:"floralwhite"}}>
+                Team
+              </a>
+            </li>
+            <li>
+              <a href='#contact' className='page-scroll' style={{color:"floralwhite"}}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
